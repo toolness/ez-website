6c5d6aebcf947d88e1c5afe00f064e9b870045c9
Atul Varma <varmaa@gmail.com>
2022-01-30 16:24:11 -0500

    Render rich text and links.

--- lib/templating/block.tsx
+++ lib/templating/block.tsx
@@ -1,6 +1,6 @@
-import { Block } from "@notionhq/client/build/src/api-types";
+import type { Block, RichText } from "@notionhq/client/build/src/api-types";
 import React from "react";
-import { joinRichTextPlaintext } from "../notion-util.js";
+import { StaticRenderer } from "./static-renderer.js";
 import { Warning } from "./warning.js";
 
 export type NotionBlockProps = {
@@ -9,8 +9,64 @@ export type NotionBlockProps = {
 
 export const NotionBlock: React.FC<NotionBlockProps> = ({ data }) => {
   if (data.type === "paragraph") {
-    // TODO: Render actual rich text, not plaintext.
-    return <p>{joinRichTextPlaintext(data.paragraph.text)}</p>;
+    return <p>{renderRichText(data.paragraph.text)}</p>;
   }
   return <Warning text={`Unknown block type "${data.type}"`} />;
 };
+
+function keyedFragment(
+  key: number,
+  children: JSX.Element | string
+): JSX.Element {
+  return <React.Fragment key={key}>{children}</React.Fragment>;
+}
+
+function renderRichText(richTexts: RichText[]): JSX.Element[] {
+  return richTexts.map((richText, i) => {
+    if (richText.type !== "text") {
+      StaticRenderer.current.logWarning(
+        `Don't know how to render "${richText.type}"`
+      );
+      return keyedFragment(i, richText.plain_text);
+    }
+
+    const { text } = richText;
+    let current: JSX.Element | string = text.content;
+
+    if (text.link) {
+      current = (
+        <a key={i} href={text.link.url} target="_blank">
+          {current}
+        </a>
+      );
+    }
+
+    if (richText.annotations) {
+      const a = richText.annotations;
+      if (a.bold) {
+        current = <strong>{current}</strong>;
+      }
+      if (a.code) {
+        current = <code>{current}</code>;
+      }
+      if (a.italic) {
+        current = <em>{current}</em>;
+      }
+      if (a.strikethrough) {
+        current = <s>{current}</s>;
+      }
+      if (a.underline) {
+        current = (
+          <span style={{ textDecoration: "underline" }}>{current}</span>
+        );
+      }
+      if (a.color !== "default") {
+        StaticRenderer.current.logWarning(
+          `Don't know how to render color "${a.color}".`
+        );
+      }
+    }
+
+    return keyedFragment(i, current);
+  });
+}
