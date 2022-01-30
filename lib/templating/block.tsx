0025ca3a4695e39bc392d758ff903c53e0447fd5
Atul Varma <varmaa@gmail.com>
2022-01-30 17:19:21 -0500

    Render lists.

--- lib/templating/block.tsx
+++ lib/templating/block.tsx
@@ -1,15 +1,76 @@
 import type { Block, RichText } from "@notionhq/client/build/src/api-types";
 import React from "react";
+import { assertUnreachable } from "../util.js";
 import { StaticRenderer } from "./static-renderer.js";
 import { Warning } from "./warning.js";
+
+export type NotionBlocksProps = {
+  blocks: Block[];
+};
+
+type InProgressList = {
+  type: "bulleted_list_item" | "numbered_list_item";
+  key: string;
+  items: JSX.Element[];
+};
+
+export const NotionBlocks: React.FC<NotionBlocksProps> = ({ blocks }) => {
+  const result: JSX.Element[] = [];
+  let currentList: InProgressList | undefined;
+  const tryToCloseCurrentList = () => {
+    if (!currentList) return;
+    if (currentList.type === "bulleted_list_item") {
+      result.push(<ul key={currentList.key}>{currentList.items}</ul>);
+    } else if (currentList.type === "numbered_list_item") {
+      result.push(<ol key={currentList.key}>{currentList.items}</ol>);
+    } else {
+      assertUnreachable(currentList.type, `unexpected list type`);
+    }
+    currentList = undefined;
+  };
+
+  for (const block of blocks) {
+    if (
+      block.type === "bulleted_list_item" ||
+      block.type === "numbered_list_item"
+    ) {
+      if (!(currentList && currentList.type === block.type)) {
+        tryToCloseCurrentList();
+        currentList = { type: block.type, items: [], key: block.id };
+      }
+      currentList.items.push(<NotionBlock key={block.id} data={block} />);
+    } else {
+      tryToCloseCurrentList();
+      result.push(<NotionBlock key={block.id} data={block} />);
+    }
+  }
+
+  tryToCloseCurrentList();
+
+  return <>{result}</>;
+};
 
 export type NotionBlockProps = {
   data: Block;
 };
 
-export const NotionBlock: React.FC<NotionBlockProps> = ({ data }) => {
+const NotionBlock: React.FC<NotionBlockProps> = ({ data }) => {
   if (data.type === "paragraph") {
     return <p>{renderRichText(data.paragraph.text)}</p>;
+  } else if (data.type === "bulleted_list_item") {
+    return (
+      <li>
+        {renderRichText(data.bulleted_list_item.text)}
+        <NotionBlocks blocks={data.bulleted_list_item.children || []} />
+      </li>
+    );
+  } else if (data.type === "numbered_list_item") {
+    return (
+      <li>
+        {renderRichText(data.numbered_list_item.text)}
+        <NotionBlocks blocks={data.numbered_list_item.children || []} />
+      </li>
+    );
   }
   return <Warning text={`Unknown block type "${data.type}"`} />;
 };
--- lib/templating/notion-page-asset-content.tsx
+++ lib/templating/notion-page-asset-content.tsx
@@ -1,15 +1,9 @@
 import React from "react";
 import { NotionPageAsset } from "../assets.js";
-import { NotionBlock } from "./block.js";
+import { NotionBlocks } from "./block.js";
 
 export const NotionPageAssetContent: React.FC<{ data: NotionPageAsset }> = ({
   data,
 }) => {
-  return (
-    <>
-      {data.content.map((block) => {
-        return <NotionBlock key={block.id} data={block} />;
-      })}
-    </>
-  );
+  return <NotionBlocks blocks={data.content} />;
 };
